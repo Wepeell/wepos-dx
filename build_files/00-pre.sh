86689776910b6d0ffb1afb79f4381cf3719a8f4e
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-03-16 17:48:39 +0100

    Add 00-pre.sh

---
+++ build_files/00-pre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Remove following error when installing packages from repos:
+# gpg: Fatal: can't create directory '/root/.gnupg': No such file or directory
+mkdir -p /var/roothome
+
+# Allow packages to install files into /opt
+# These files needs to be copied into an immutable part of the image
+mkdir -p /var/opt
+
+# Disable Terra repo
+dnf5 -y config-manager setopt "terra*".enabled=false
+# This should fix Anaconda ISO building
+sed -i 's/^enabled=.*/enabled=0/' /etc/yum.repos.d/terra-mesa.repo

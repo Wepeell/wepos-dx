8f7cdcade5169006ee4473cdaddffd0c58df174e
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-03-16 17:49:44 +0100

    Add 99-post.sh

---
+++ build_files/99-post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Clean package manager cache
+dnf5 clean all
+
+# Copy system_files to base image
+cp -r /ctx/system_files/* /
+
+# Remove autostart files
+rm -f /etc/skel/.config/autostart/steam.desktop
+
+### Add kargs
+# cat <<'EOF' > "/usr/lib/bootc/kargs.d/99-amd.toml"
+# kargs = ["amdgpu.ppfeaturemask=0xffffffff"]
+# EOF

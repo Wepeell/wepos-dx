177fefa08c2990c163c22bac262a86647d26a092
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-03-16 17:49:09 +0100

    Add 98-optfix.sh

---
+++ build_files/98-optfix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Prevent glob from being used literally as a file
+shopt -s nullglob
+
+# Variables
+VAR_OPT_DIR="/var/opt"
+LIB_OPTFIX_DIR="/usr/lib/optfix"
+TMPFILESD_CONF="/usr/lib/tmpfiles.d/optfix.conf"
+
+# Check if /var/opt has content
+if compgen -G "${VAR_OPT_DIR}/*" > /dev/null; then
+	# Move /var/opt to /usr/lib/optfix
+	mv "${VAR_OPT_DIR}" "${LIB_OPTFIX_DIR}"
+
+	# Generate tmpfiles.d config
+	# Creates symlinks from /var/opt/<installed folder> to /usr/lib/optfix/<installed folder> on boot
+	for DIR in ${LIB_OPTFIX_DIR}/*; do
+		NAME_DIR=$(basename "$DIR")
+		echo "L+? '${VAR_OPT_DIR}/${NAME_DIR}' - - - - ${LIB_OPTFIX_DIR}/${NAME_DIR}" >> "${TMPFILESD_CONF}"
+	done
+fi

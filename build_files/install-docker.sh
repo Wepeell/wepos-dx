e39b5edff67adea6d62623dfa616a3c879c3da1f
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-03-16 17:50:25 +0100

    Add install-docker.sh

---
+++ build_files/install-docker.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Links
+# https://github.com/ublue-os/bazzite-dx/blob/main/build_files/20-install-apps.sh
+# https://docs.docker.com/engine/install/fedora/
+# https://docs.docker.com/engine/install/linux-postinstall
+
+# Packages
+docker_pkgs=(
+    containerd.io
+    docker-buildx-plugin
+    docker-ce
+    docker-ce-cli
+    docker-compose-plugin
+)
+
+# Add Docker repo
+dnf5 config-manager addrepo --from-repofile="https://download.docker.com/linux/fedora/docker-ce.repo"
+
+# Disable repo
+dnf5 config-manager setopt docker-ce-stable.enabled=0
+
+# Temporarily enable repo and install Docker packages
+dnf5 install -y --enable-repo="docker-ce-stable" "${docker_pkgs[@]}" || {
+    # Use test packages if docker pkgs is not available for f42
+    if (($(lsb_release -sr) == 42)); then
+        echo "::info::Missing docker packages in f42, falling back to test repos..."
+        dnf5 install -y --enablerepo="docker-ce-test" "${docker_pkgs[@]}"
+    fi
+}
+
+# Load iptable_nat module for docker-in-docker.
+# See:
+#   - https://github.com/ublue-os/bluefin/issues/2365
+#   - https://github.com/devcontainers/features/issues/1235
+mkdir -p /etc/modules-load.d && cat >>/etc/modules-load.d/ip_tables.conf <<EOF
+iptable_nat
+EOF
+
+# Start Docker Engine service
+systemctl enable --now docker

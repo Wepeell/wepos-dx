12cba12fe92941e913568b4e06d0d598f56b69c3
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-03-16 17:48:01 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -21,4 +21,36 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
+
+
+
+### Install scripts
+
+# Run script function
+function run_script() {
+	local script="$1"
+
+	echo "Running $script..."
+
+	if "$script"; then
+		echo "$script completed!"
+	else
+		echo "$script failed! Exiting."
+		exit 1
+	fi
+}
+
+# Build scripts array
+# Target scripts as elements
+scripts=(
+	/ctx/00-pre.sh
+	/ctx/install-docker
+	/ctx/98-optfix.sh
+	/ctx/99-post.sh
+)
+
+# Run each script in array
+for script_element in "${scripts[@]}"; do
+	run_script "$script_element"
+done

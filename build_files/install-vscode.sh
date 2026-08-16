804fce278a9ca632b6494d4a579a1814b6af4df0
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-08-16 03:31:24 +0200

    clean up install-vscode.sh

--- build_files/install-vscode.sh
+++ build_files/install-vscode.sh
@@ -6,27 +6,26 @@ set -ouex pipefail
 # https://code.visualstudio.com/docs/setup/linux
 # hhttps://github.com/ublue-os/bazzite-dx/blob/b4ac4255418da91567a920724059807fe4d8e36d/build_files/20-install-apps.sh
 
-### Repo base URL
+# Repo base URL
 repo_base_url="https://packages.microsoft.com/yumrepos/vscode"
 
-### Repo ID
+# Repo ID
 repo_id="vscode"
 
-### Packages array
+# Packages to install
 packages=(
     code
 )
 
-### Enable repo
+# Enable repo
 dnf5 -y config-manager addrepo --set=baseurl="$repo_base_url" --id="$repo_id"
 
 # FIXME: gpgcheck is broken for vscode due to it using `asc` for checking
 # seems to be broken on newer rpm security policies.
 dnf5 config-manager setopt vscode.gpgcheck=0
 
-### Check if base image packages are being replaced
-# Dry run
-dnf5 -y install --setopt=tsflags=test --nogpgcheck "${packages[@]}" 2>&1 | tee /tmp/dryrun.log
+# Check if base image packages are being replaced with a dry run
+dnf5 --setopt=tsflags=test -y install "${packages[@]}" 2>&1 | tee /tmp/dryrun.log
 
 # Check log for upgrading and downgrading
 if grep -qE '^(Upgrading|Downgrading):' /tmp/dryrun.log; then
@@ -34,8 +33,8 @@ if grep -qE '^(Upgrading|Downgrading):' /tmp/dryrun.log; then
     exit 1
 fi
 
-### Install packages
-dnf5 -y install --nogpgcheck "${packages[@]}"
+# Install packages
+dnf5 --nogpgcheck -y install "${packages[@]}"
 
-### Disable repo
+# Disable repo
 dnf5 -y config-manager setopt "*${repo_id}*".enabled=false

9648e1ef5663ceaf52c196cbe5cf8b75faf8c04c
Wepeell <4354676+Wepeell@users.noreply.github.com>
2026-09-08 17:31:17 +0200

    fix localwp not installing

--- build_files/install-localwp.sh
+++ build_files/install-localwp.sh
@@ -13,6 +13,10 @@ packages=(
 # Check if base image packages are being replaced with a dry run
 dnf5 --setopt=tsflags=test -y install "${packages[@]}" 2>&1 | tee /tmp/dryrun.log
 
+# Clear cache to prevent this error:
+# Failed to load RPM "/var/cache/libdnf5/@commandline-f1fcf1ce45c07955/packages/b33d97cee1a1c6ef-local-10.1.2-linux.rpm": /var/cache/libdnf5/@commandline-f1fcf1ce45c07955/packages/b33d97cee1a1c6ef-local-10.1.2-linux.rpm: not a rpm
+rm -rf /var/cache/libdnf5/@commandline-*
+
 # Check log for upgrading and downgrading
 if grep -qE '^(Upgrading|Downgrading):' /tmp/dryrun.log; then
     echo "::notice::Detected package replacements. Aborting build."
